ec154dfbbc9e071dd550b0d8bba24a8c3fd89fe0
John McCormack <mccormack.wgsi@gmail.com>
2015-04-16 14:00:41 -0400

    Working on noise removal

--- sudoku.py
+++ sudoku.py
@@ -4,3 +4,22 @@ import numpy as np
 import math
 from matplotlib import pyplot as plt
 
+class Sudoku:
+    
+    def __init__(self, imagepath):
+        self.image = cv2.imread(imagepath)
+        self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        self._noiseRemove()
+        
+    def _noiseRemove(self):
+        blurred = cv2.GaussianBlur(self.image,(5,5), 0)
+        unsharp = self.image - blurred
+        self.image = self.image + unsharp
+        self.image = cv2.Canny(self.image, 100,200)
+        cv2.imwrite("output.jpg", self.image)
+        
+        
+        
+
+if __name__ == '__main__':
+    Sudoku("sudoku.jpg")
